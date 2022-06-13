85ba310c576446541b7ceda09e6740c13030df62
root <root@Ajneo.localdomain>
2022-06-13 15:30:36 +0200

    first commit

--- app/concate.js
+++ app/concate.js
@@ -1,9 +1,6 @@
 const lodash = require('lodash')
-
 const arr1 = [6, 7, 8, 9, 10]
-
 const arraystogether = (arr) => {
     return lodash.concat(arr, arr1);
 }
-
 module.exports.arraystogether = arraystogether;
--- app/sum.js
+++ app/sum.js
@@ -1,5 +1,4 @@
 const sum = (arr) => {
     return arr.reduce(function (a, b) { return a + b; }, 0);;
 }
-
 module.exports.sum = sum;
