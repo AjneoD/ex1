9e08480b49883b65326ff10e0d55d55dc3ca052e
root <root@Ajneo.localdomain>
2022-06-14 12:08:21 +0200

    async function

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const fs = require('fs')
 const concat = require('./app/concate')
 const sum = require('./app/sum')
+
 const details = JSON.parse(fs.readFileSync('array.json', 'utf8')).array;
 const con = concat.arraystogether(details);
 const sumTotale = sum.sum(con);
+
+function resolveAfter2Seconds() {
+
+    return new Promise(arraystogether => {
+
+        setTimeout(() => {
+
+            arraystogether(con);
+
+        }, 2000);
+
+    });
+
+}
+
+async function asyncCall() {
+
+    console.log(details);
+
+    const result = await resolveAfter2Seconds();
+
+    console.log(result);
+
+}
+
+asyncCall();
+  
 
 console.log(details);
 console.log(con);
